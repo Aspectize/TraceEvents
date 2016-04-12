2479ca1656bc877cf8bb8b4142c55601f722bea0
Fredy <fredy@aspectize.com>
2016-04-12 13:50:25 +0200

    FF

--- TraceEvents/TraceService.cs
+++ TraceEvents/TraceService.cs
@@ -6,6 +6,10 @@ using Aspectize.Core;
 using TraceEvents;
 using System.Web;
 using System.Security.Permissions;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
 
 namespace TraceMyApps
 {
@@ -21,8 +25,7 @@ namespace TraceMyApps
     }
 
     [Service(Name = "TraceEventsConfigurableService", ConfigurationRequired = true)]
-    public class TraceService : ITrace, IInitializable
-    {
+    public class TraceService : ITrace, IInitializable {
         const string ExtractFormatDateYear = "yyyy";
         const string ExtractFormatDateQuarter = "yyyyQ";
         const string ExtractFormatDateMonth = "yyyyQMM";
@@ -35,9 +38,13 @@ namespace TraceMyApps
         [Parameter(Optional = false)]
         string DataServiceName = "";
 
+        [Parameter(Optional = false)]
+        string QueueStorage = "";
+
+
+
         [PrincipalPermission(SecurityAction.Demand, Authenticated = true)]
-        DataSet ITrace.LoadTraces()
-        {
+        DataSet ITrace.LoadTraces () {
             IDataManager dm = EntityManager.FromDataBaseService(DataServiceName);
 
             IEntityManager em = dm as IEntityManager;
@@ -52,16 +59,14 @@ namespace TraceMyApps
             dm.LoadEntitiesGraph<Root>(roleRelationQuerys, root.Id);
 
             var now = DateTime.UtcNow;
-            foreach (Events events in em.GetAllInstances<Events>())
-            {
+            foreach (Events events in em.GetAllInstances<Events>()) {
                 BuildStat(dm, root.Id, events, now);
             }
 
             return dm.Data;
         }
 
-        internal static void BuildStat(IDataManager dm, Guid rootId, Events events, DateTime dateTime)
-        {
+        internal static void BuildStat (IDataManager dm, Guid rootId, Events events, DateTime dateTime) {
             IEntityManager em = dm as IEntityManager;
 
             Stat stat = em.CreateInstance<Stat>();
@@ -79,18 +84,16 @@ namespace TraceMyApps
             //BuildTagStat<Minute, TagMinute>(dm, rootId, events, stat, dateTime, TraceService.ExtractFormatDateMinute);
         }
 
-        internal static void BuildStat<T, R>(IDataManager dm, Guid rootId, Events events, Stat stat, DateTime dateTime, string datePeriodFormat)
+        internal static void BuildStat<T, R> (IDataManager dm, Guid rootId, Events events, Stat stat, DateTime dateTime, string datePeriodFormat)
             where T : Entity, IEntity, IDataWrapper, new()
-            where R : DataWrapper, IDataWrapper, IRelation, new()
-        {
+            where R : DataWrapper, IDataWrapper, IRelation, new() {
             IEntityManager em = dm as IEntityManager;
 
             string periodId = TraceService.GetTimeIdFromDate(rootId, events.Id, dateTime, datePeriodFormat);
 
             T t = dm.GetEntity<T>(periodId);
 
-            if (t != null)
-            {
+            if (t != null) {
                 em.AssociateInstance<R>(t, events);
 
                 string periodName = typeof(T).Name;
@@ -138,8 +141,7 @@ namespace TraceMyApps
         //    return dm.Data;
         //}
 
-        DataSet ITrace.GetEventsHistory(Guid eventsId, Date dateStart, Date dateEnd, EnumFrequency frequency)
-        {
+        DataSet ITrace.GetEventsHistory (Guid eventsId, Date dateStart, Date dateEnd, EnumFrequency frequency) {
             IDataManager dm = EntityManager.FromDataBaseService(DataServiceName);
 
             var root = GetRoot(dm, false);
@@ -164,8 +166,7 @@ namespace TraceMyApps
 
             List<Month> tagMonths = dm.GetEntities<Month>(qcMonth);
 
-            foreach (Month m in tagMonths)
-            {
+            foreach (Month m in tagMonths) {
                 em.AssociateInstance<EventsMonth>(events, m);
             }
 
@@ -174,8 +175,7 @@ namespace TraceMyApps
 
             List<Week> tagWeeks = dm.GetEntities<Week>(qcWeek);
 
-            foreach (Week w in tagWeeks)
-            {
+            foreach (Week w in tagWeeks) {
                 em.AssociateInstance<EventsWeek>(events, w);
             }
 
@@ -184,8 +184,7 @@ namespace TraceMyApps
 
             List<Day> days = dm.GetEntities<Day>(qcDay);
 
-            foreach (Day d in days)
-            {
+            foreach (Day d in days) {
                 em.AssociateInstance<EventsDay>(events, d);
             }
 
@@ -193,12 +192,10 @@ namespace TraceMyApps
 
             TimeSpan ts = new Date(dateEndTomorrow) - dateStart;
 
-            for (var i = 0; i < ts.Days; i++)
-            {
+            for (var i = 0; i < ts.Days; i++) {
                 string tempDayId = TraceService.GetTimeIdFromDate(root.Id, events.Id, temp, TraceService.ExtractFormatDateDay);
 
-                if (!events.Day.Exists(item => item.Id == tempDayId))
-                {
+                if (!events.Day.Exists(item => item.Id == tempDayId)) {
                     Day tempTagDay = em.CreateInstance<Day>();
                     tempTagDay.Id = tempDayId;
                     tempTagDay.TimeKey = tempDayId.Split('|')[2]; //TraceService.GetTimeKey(temp);
@@ -208,8 +205,7 @@ namespace TraceMyApps
 
                 string tempWeekId = TraceService.GetTimeIdFromDate(root.Id, events.Id, temp, TraceService.ExtractFormatDateWeek);
 
-                if (!events.Week.Exists(item => item.Id == tempWeekId))
-                {
+                if (!events.Week.Exists(item => item.Id == tempWeekId)) {
                     Week tempTagWeek = em.CreateInstance<Week>();
                     tempTagWeek.Id = tempWeekId;
                     tempTagWeek.TimeKey = tempWeekId.Split('|')[2]; //TraceService.GetTimeKey(temp);
@@ -219,8 +215,7 @@ namespace TraceMyApps
 
                 string tempMonthId = TraceService.GetTimeIdFromDate(root.Id, events.Id, temp, TraceService.ExtractFormatDateMonth);
 
-                if (!events.Month.Exists(item => item.Id == tempMonthId))
-                {
+                if (!events.Month.Exists(item => item.Id == tempMonthId)) {
                     Month tempTagMonth = em.CreateInstance<Month>();
                     tempTagMonth.Id = tempMonthId;
                     tempTagMonth.TimeKey = tempMonthId.Split('|')[2]; //TraceService.GetTimeKey(temp);
@@ -236,22 +231,113 @@ namespace TraceMyApps
             return dm.Data;
         }
 
+        static bool eventsQueueCreated = false;
+        const string eventsQueue = "events";
 
-        void ITrace.Events(string eventName, string eventValue, string userId, string info)
-        {
+        const string fEventName = "eventName";
+        const string fEventValue = "eventValue";
+        const string fUserId = "userId";
+        const string fInfo = "info";
+        const string fUserAgent = "userAgent";
+        const string fDatetiem = "dt";
+
+        static CloudQueue getCloudQueue (string queueStorage) {
+
+            var csa = CloudStorageAccount.Parse(queueStorage);
+            var qc = csa.CreateCloudQueueClient();
+
+            CloudQueue q = qc.GetQueueReference(eventsQueue);
+
+            if (!eventsQueueCreated) {
+
+                lock(eventsQueue) {
+
+                    q.CreateIfNotExists();
+                    eventsQueueCreated = true;
+                }
+            }
+
+            return q;
+        }
+
+        static bool processorRunning = false;
+
+        static void processor (string queueStorage, string dataServiceName) {
+
+            var q = getCloudQueue(queueStorage);
+
+            while (true) {
+
+                processorRunning = true;
+
+                var m = q.GetMessage(TimeSpan.FromSeconds(60));
+
+                if (m != null) {
+
+                    try {
+
+                        var data = JValue.Parse(m.AsString);
+
+                        var eventName = data.Value<string>(fEventName);
+                        var eventValue = data.Value<string>(fEventValue);
+                        var userId = data.Value<string>(fUserId);
+                        var info = data.Value<string>(fInfo);
+                        var userAgent = data.Value<string>(fUserAgent);
+                        var dt = data.Value<DateTime>(fDatetiem);
+
+                        handleMessage(dataServiceName, dt, eventName, eventValue, userId, info, userAgent);
+
+                        q.DeleteMessage(m);
+
+                    } catch (Exception x) {
+
+                       Context.LogException(x);
+                    }
+
+                } else System.Threading.Thread.Sleep(1000);                
+            }
+        }
+
+        void sendMessage (string message) {
+
+            var m = new CloudQueueMessage(message);
+
+            var q = getCloudQueue(QueueStorage);
+
+            q.AddMessageAsync(m);
+        }
+       
+        string buildMessage (string eventName, string eventValue, string userId, string info, string userAgent) {
+
+            var dt = DateTime.UtcNow;
+
+            var jsonObject = new JObject();
+
+            jsonObject.Add(fDatetiem, dt);
+            jsonObject.Add(fEventName, eventName);
+            jsonObject.Add(fEventValue, eventValue);
+            jsonObject.Add(fUserId, userId);
+            jsonObject.Add(fInfo, info);
+            jsonObject.Add(fUserAgent, userAgent);
+
+            return jsonObject.ToString();
+        }
+
+        static void handleMessage (string dsn, DateTime dt, string eventName, string eventValue, string userId, string info, string userAgent) {
+
+            var DataServiceName = dsn;
+
             IDataManager dm = EntityManager.FromDataBaseService(DataServiceName);
 
             IEntityManager em = dm as IEntityManager;
 
-            DateTime traceDate = DateTime.UtcNow;
+            DateTime traceDate = dt;
 
             decimal? decimalValue = null;
 
-            if (!string.IsNullOrEmpty(eventValue))
-            {
+            if (!string.IsNullOrEmpty(eventValue)) {
                 decimal decimalValueNonNull;
-                if (decimal.TryParse(eventValue, out decimalValueNonNull))
-                {
+                if (decimal.TryParse(eventValue, out decimalValueNonNull)) {
                     decimalValue = decimalValueNonNull;
                 }
             }
@@ -260,15 +346,13 @@ namespace TraceMyApps
 
             Who who = null;
 
-            if (string.IsNullOrEmpty(userId))
-            {
+            if (string.IsNullOrEmpty(userId)) {
                 userId = Guid.Empty.ToString("N");
             }
 
             who = dm.GetEntity<Who>(userId);
 
-            if (who == null)
-            {
+            if (who == null) {
                 who = em.CreateInstance<Who>();
 
                 who.Id = userId;
@@ -276,16 +360,29 @@ namespace TraceMyApps
                 em.AssociateInstance<RootWho>(who, root);
             }
 
-            who.DateLastTrace = DateTime.UtcNow;
+            who.DateLastTrace = dt;
 
             string[] eventslist = eventName.Split('|');
 
-            foreach (string name in eventslist)
-            {
+            foreach (string name in eventslist) {
                 BuildTrace(root, decimalValue, who, null, null, info, dm, traceDate, name.Trim());
             }
 
             dm.SaveTransactional();
+        }
+
+        void ITrace.Events (string eventName, string eventValue, string userId, string info) {
+
+            var userAgent = (HttpContext.Current != null && HttpContext.Current.Request != null) ? HttpContext.Current.Request.UserAgent : String.Empty;
+
+            var message = buildMessage(eventName, eventValue, userId, info, userAgent);
+
+            sendMessage(message);
+
+            if (!processorRunning) {
+
+                ExecutingContext.RunProcessor("P", () => { processor(QueueStorage, DataServiceName); });
+            }
         }
 
         internal static string GetTimeIdFromDate(Guid rootId, Guid eventsId, DateTime dateTime, string extractFormat)
@@ -456,7 +553,6 @@ namespace TraceMyApps
 
             return em.GetAllInstances<Root>()[0];
         }
-
 
         void IInitializable.Initialize(Dictionary<string, object> parameters)
         {
